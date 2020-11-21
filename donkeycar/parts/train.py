d3ce377bed8853964e152c78430f610c547422ab
DocGarbanzo <dirk.prange@web.de>
2020-11-21 21:46:04 +0000

    Move training from template to own part (actually part might be misleading here, as it's not really a part). Implement 'donkey train --model --tub --type' to replace 'train.py --model --tub --type'.

--- donkeycar/parts/train.py
+++ donkeycar/parts/train.py
@@ -1,239 +1,128 @@
 import os
+import random
+from pathlib import Path
+
+import cv2
 import numpy as np
-from abc import ABC, abstractmethod
-from collections.abc import Sequence
-from typing import List, Dict, Any, Tuple, Union
-from tensorflow.python.keras.utils.data_utils import Sequence as TfSequence
+from docopt import docopt
+from PIL import Image
+from tensorflow.python.keras.callbacks import EarlyStopping, ModelCheckpoint
+from tensorflow.python.keras.utils.data_utils import Sequence
+
+import donkeycar
+from donkeycar.parts.keras import KerasInferred, KerasCategorical
+from donkeycar.parts.tflite import keras_model_to_tflite
 from donkeycar.parts.tub_v2 import Tub
 from donkeycar.utils import get_model_by_type, load_image_arr, \
-    train_test_split, normalize_image
-from donkeycar.parts.keras import KerasPilot
-from donkeycar.config import Config
-
-# for typing
-Record = Dict[str, Any]
-Records = List[Record]
-
-
-DEFAULT_TRANSFORMATIONS = ['ImageReader', 'ImageNormalizer']
+    train_test_split, linear_bin, normalize_image
 
 
 class TubDataset(object):
-    """
+    '''
     Loads the dataset, and creates a train/test split.
-    """
+    '''
 
-    def __init__(self,
-                 tub_paths: List[str],
-                 test_size: float = 0.2,
-                 shuffle: bool = True) -> None:
-        """
-        :param tub_paths:   list of paths to tubs
-        :param test_size:   test / train split
-        :param shuffle:     if shuffling the data or not
-        """
+    def __init__(self, tub_paths, test_size=0.2, shuffle=True):
         self.tub_paths = tub_paths
         self.test_size = test_size
         self.shuffle = shuffle
         self.tubs = [Tub(tub_path, read_only=True) for tub_path in
                      self.tub_paths]
-        self.records: Records = list()
+        self.records = list()
 
-    def train_test_split(self) -> Tuple[Records, Records]:
-        print('Loading tubs from paths %s' % self.tub_paths)
+    def train_test_split(self):
+        print('Loading tubs from paths %s' % (self.tub_paths))
         for tub in self.tubs:
             for record in tub:
                 record['_image_base_path'] = tub.images_base_path
                 self.records.append(record)
 
-        return train_test_split(self.records, shuffle=self.shuffle,
-                                test_size=self.test_size)
-
-
-class RecordTransformer(ABC):
-    """ Base class for record transformations which can be stacked on top of
-        each other """
-
-    def __init__(self,
-                 key: str,
-                 config: Config,
-                 cache: bool = False) -> None:
-        """
-        :param key:     key on which the transformation should apply
-        :param config:  donkey config
-        :param cache:   if transformed record should be put back for
-                        performance - this destroys the original record
-        """
-        self.config = config
-        self.key = key
-        self.cache = cache
-
-    def get(self, record: Record, key: str, val: Any) -> Any:
-        """ Override the base class to allow transforming and caching of
-            transformed entries. """
-        if key == self.key:
-            val_trans = self.transform(record, val)
-            if self.cache:
-                record[key] = val_trans
-            return val_trans
-        else:
-            return val
-
-    @abstractmethod
-    def transform(self, record: Record, val: Any) -> Any:
-        """ This has to be implemented in derived classes"""
-        pass
-
-    @classmethod
-    def create(cls,
-               transform_names: List[str],
-               config: Config) -> List["RecordTransformer"]:
-        """ Method to create a stack of transformations on top of the
-            LazyRecord """
-        transforms = [globals()[name](config) for name in transform_names]
-        return transforms
-
-
-class ImageReader(RecordTransformer):
-    """ Convert path into image array """
-
-    def __init__(self, config: Config) -> None:
-        super().__init__('cam/image_array', config, True)
-
-    def transform(self, record: Record, val: Any) -> Any:
-        if type(val) is str:
-            base_path = record['_image_base_path']
-            # only transform once into numpy img, when value is path to image
-            image_path = os.path.join(base_path, val)
-            image = load_image_arr(image_path, self.config)
-            return image
-        else:
-            return val
-
-
-class ImageNormalizer(RecordTransformer):
-    """ Normalize Images from np.uint8 to np.float32. We don't want to cache
-        these as they require 4x memory. """
-
-    def __init__(self, config: Config) -> None:
-        super().__init__('cam/image_array', config, False)
-
-    def transform(self, record: Record, val: np.ndarray) -> np.ndarray:
-        return normalize_image(val)
-
-
-class LazyRecord(object):
-    """ Lazy record which wraps around record dictionary. There is no
-        additional functionality here, it's all in the derived classes. """
-
-    def __init__(self,
-                 record: Record,
-                 model: KerasPilot,
-                 transforms: List[RecordTransformer]) -> None:
-        self.model = model
-        self.record = record
-        self.transforms = transforms
-
-    def get_x(self) -> Any:
-        """ Dispatch returning of X to the model which will call
-            get_entry() on the LazyRecord and from there drive the
-            transformations. """
-        return self.model.lazy_record_transform_x(self)
-
-    def get_y(self) -> Any:
-        """ Dispatch returning of Y to the model which will call
-            get_entry() on the LazyRecord and from there drive the
-            transformations. """
-        return self.model.lazy_record_transform_y(self)
-
-    def get_entry(self, key: str) -> Any:
-        """ Get entry from record and run through transformations """
-        val = self.record.get(key)
-        for transform in self.transforms:
-            val = transform.get(self.record, key, val)
-        return val
+        return train_test_split(self.records, shuffle=self.shuffle, test_size=self.test_size)
 
 
 class TubSequence(Sequence):
-    """ Converts sequence of records to lazy records. """
-    def __init__(self,
-                 model: KerasPilot,
-                 config: Config,
-                 records: Records,
-                 is_train: bool = True) -> None:
-        self.model = model
+    def __init__(self, keras_model, config, records=list()):
+        self.keras_model = keras_model
         self.config = config
         self.records = records
-        cfg_attr = 'TRAIN_TRANSFORMATIONS' if is_train else \
-            'VALIDATION_TRANSFORMATIONS'
-        transforms = getattr(self.config, cfg_attr, DEFAULT_TRANSFORMATIONS)
-        self.transformations = RecordTransformer.create(transforms, self.config)
+        self.batch_size = self.config.BATCH_SIZE
 
-    def __len__(self) -> int:
-        return len(self.records)
+    def __len__(self):
+        return len(self.records) // self.batch_size
 
-    def __getitem__(self, index: int) -> LazyRecord:
-        record = self.records[index]
-        lazy_record = LazyRecord(record, self.model, self.transformations)
-        return lazy_record
-
-
-class BatchSequence(TfSequence):
-    def __init__(self,
-                 lazy_records: TubSequence,
-                 batch_size: int) -> None:
-        self.lazy_records = lazy_records
-        self.batch_size = batch_size
-
-    def __len__(self) -> float:
-        return len(self.lazy_records) // self.batch_size
-
-    def __getitem__(self, index: int) -> Tuple[Any, Any]:
+    def __getitem__(self, index):
         count = 0
-        x = []
-        y = []
-        # collecting across the whole batch
+        records = []
+        images = []
+        angles = []
+        throttles = []
+
+        is_inferred = type(self.keras_model) is KerasInferred
+        is_categorical = type(self.keras_model) is KerasCategorical
+
         while count < self.batch_size:
             i = (index * self.batch_size) + count
-            if i >= len(self.lazy_records):
+            if i >= len(self.records):
                 break
-            this_record = self.lazy_records[i]
-            single_x = this_record.get_x()
-            single_y = this_record.get_y()
-            x.append(single_x)
-            y.append(single_y)
+
+            record = self.records[i]
+            record = self._transform_record(record)
+            records.append(record)
             count += 1
 
-        # reshape X, Y
-        def reshape(z):
-            # each entry in z could either be a single value, or a numpy
-            # array, or a tuple containing values and numpy arrays
-            if type(z[0]) is tuple:
-                dim = len(z[0])
-                ret_z = []
-                for j in range(dim):
-                    z_j = np.array([zi[j] for zi in z])
-                    ret_z.append(z_j)
-                return ret_z
-            else:
-                return np.array(z)
+        for record in records:
+            image = record['cam/image_array']
+            angle = record['user/angle']
+            throttle = record['user/throttle']
 
-        x_res = reshape(x)
-        y_res = reshape(y)
-        return x_res, y_res
+            images.append(image)
+            # for categorical convert to one-hot vector
+            if is_categorical:
+                R = self.config.MODEL_CATEGORICAL_MAX_THROTTLE_RANGE
+                angle = linear_bin(angle, N=15, offset=1, R=2.0)
+                throttle = linear_bin(throttle, N=20, offset=0.0, R=R)
+            angles.append(angle)
+            throttles.append(throttle)
+
+        X = np.array(images)
+
+        if is_inferred:
+            Y = np.array(angles)
+        else:
+            Y = [np.array(angles), np.array(throttles)]
+
+        return X, Y
+
+    def _transform_record(self, record):
+        for key, value in record.items():
+            if key == 'cam/image_array' and isinstance(value, str):
+                image_path = os.path.join(record['_image_base_path'], value)
+                image = load_image_arr(image_path, self.config)
+                record[key] = normalize_image(image)
+
+        return record
 
 
-def train(cfg: Config,
-          tub_paths: Union[str, List[str]],
-          output_path: str,
-          model_type: str) -> Dict[str, Any]:
+class ImagePreprocessing(Sequence):
+    '''
+    A Sequence which wraps another Sequence with an Image Augumentation.
+    '''
+
+    def __init__(self, sequence, augmentation):
+        self.sequence = sequence
+        self.augumentation = augmentation
+
+    def __len__(self):
+        return len(self.sequence)
+
+    def __getitem__(self, index):
+        X, Y = self.sequence[index]
+        return self.augumentation.augment_images(X), Y
+
+
+def train(cfg, tub_paths, output_path, model_type):
     """
     Train the model
-    :param cfg:         donkey config
-    :param tub_paths:   single path or list of multiple paths for tubs
-    :param output_path: output model path
-    :param model_type:  model type, e.g linear, categorical, etc
-    :return:            history dictionary
     """
     # convert single path into list of one element
     if type(tub_paths) is str:
@@ -245,35 +134,45 @@ def train(cfg: Config,
         train_type = model_type
 
     kl = get_model_by_type(train_type, cfg)
+    kl.compile()
 
     if cfg.PRINT_MODEL_SUMMARY:
         print(kl.model.summary())
 
-    batch_size: int = cfg.BATCH_SIZE
-    dataset = TubDataset(tub_paths, test_size=(1. - cfg.TRAIN_TEST_SPLIT),
-                         shuffle=True)
+    batch_size = cfg.BATCH_SIZE
+    dataset = TubDataset(tub_paths, test_size=(1. - cfg.TRAIN_TEST_SPLIT))
     training_records, validation_records = dataset.train_test_split()
     print('Records # Training %s' % len(training_records))
     print('Records # Validation %s' % len(validation_records))
 
-    training = TubSequence(kl, cfg, records=training_records, is_train=True)
-    validation = TubSequence(kl, cfg, records=validation_records,
-                             is_train=False)
-    training_batch = BatchSequence(training, batch_size)
-    validation_batch = BatchSequence(validation, batch_size)
-
+    training = TubSequence(kl, cfg, training_records)
+    validation = TubSequence(kl, cfg, validation_records)
     assert len(validation) > 0, "Not enough validation data, decrease the " \
                                 "batch size or add more data."
 
-    history = kl.train(model_path=output_path,
-                       train_data=training_batch,
-                       train_steps=len(training_batch),
-                       batch_size=batch_size,
-                       validation_data=validation_batch,
-                       validation_steps=len(validation_batch),
-                       epochs=cfg.MAX_EPOCHS,
-                       verbose=cfg.VERBOSE_TRAIN,
-                       min_delta=cfg.MIN_DELTA,
-                       patience=cfg.EARLY_STOP_PATIENCE)
+    # Setup early stoppage callbacks
+    callbacks = [
+        EarlyStopping(monitor='val_loss', patience=cfg.EARLY_STOP_PATIENCE),
+        ModelCheckpoint(
+            filepath=output_path,
+            monitor='val_loss',
+            save_best_only=True,
+            verbose=1,
+        )
+    ]
 
+    history = kl.model.fit(
+        x=training,
+        steps_per_epoch=len(training),
+        batch_size=batch_size,
+        callbacks=callbacks,
+        validation_data=validation,
+        validation_steps=len(validation),
+        epochs=cfg.MAX_EPOCHS,
+        verbose=cfg.VERBOSE_TRAIN,
+        workers=1,
+        use_multiprocessing=False
+    )
     return history
+
+
