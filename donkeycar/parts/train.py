f05c1be10b8bdef8ce7c4de1a4d3533e55014107
DocGarbanzo <dirk.prange@web.de>
2020-12-06 22:29:14 +0000

    Fixed tf.Sequence generator for batches to correctly return a lists of batches, one entry per model output. Removed the intermediate, old-style transformations, etc.

--- donkeycar/parts/train.py
+++ donkeycar/parts/train.py
@@ -3,6 +3,9 @@ import numpy as np
 from abc import ABC, abstractmethod
 from collections.abc import Sequence
 from typing import List, Dict, Any, Tuple, Union, TypeVar, Iterable
+import math
+
+import tensorflow as tf
 from tensorflow.python.keras.utils.data_utils import Sequence as TfSequence
 from donkeycar.parts.tub_v2 import Tub
 from donkeycar.utils import get_model_by_type, load_image_arr, \
@@ -19,178 +22,15 @@ X = TypeVar('X', covariant=True)
 Y = TypeVar('Y', covariant=True)
 
 
-DEFAULT_TRANSFORMATIONS = ['ImageReader', 'ImageNormalizer']
-
-
-class TubDataset1(object):
-    """
-    Loads the dataset, and creates a train/test split.
-    """
-
+class BatchSequence(TfSequence):
     def __init__(self,
-                 tub_paths: List[str],
-                 test_size: float = 0.2,
-                 shuffle: bool = True) -> None:
-        """
-        :param tub_paths:   list of paths to tubs
-        :param test_size:   test / train split
-        :param shuffle:     if shuffling the data or not
-        """
-        self.tub_paths = tub_paths
-        self.test_size = test_size
-        self.shuffle = shuffle
-        self.tubs = [Tub(tub_path, read_only=True) for tub_path in
-                     self.tub_paths]
-        self.records: Records = list()
-
-    def train_test_split(self) -> Tuple[Records, Records]:
-        print('Loading tubs from paths %s' % self.tub_paths)
-        for tub in self.tubs:
-            for record in tub:
-                record['_image_base_path'] = tub.images_base_path
-                self.records.append(record)
-
-        return train_test_split(self.records, shuffle=self.shuffle,
-                                test_size=self.test_size)
-
-
-class RecordTransformer(ABC):
-    """ Base class for record transformations which can be stacked on top of
-        each other """
-
-    def __init__(self,
-                 key: str,
-                 config: Config,
-                 cache: bool = False) -> None:
-        """
-        :param key:     key on which the transformation should apply
-        :param config:  donkey config
-        :param cache:   if transformed record should be put back for
-                        performance - this destroys the original record
-        """
-        self.config = config
-        self.key = key
-        self.cache = cache
-
-    def get(self, record: Record, key: str, val: Any) -> Any:
-        """ Override the base class to allow transforming and caching of
-            transformed entries. """
-        if key == self.key:
-            val_trans = self.transform(record, val)
-            if self.cache:
-                record[key] = val_trans
-            return val_trans
-        else:
-            return val
-
-    @abstractmethod
-    def transform(self, record: Record, val: Any) -> Any:
-        """ This has to be implemented in derived classes"""
-        pass
-
-    @classmethod
-    def create(cls,
-               transform_names: List[str],
-               config: Config) -> List["RecordTransformer"]:
-        """ Method to create a stack of transformations on top of the
-            LazyRecord """
-        transforms = [globals()[name](config) for name in transform_names]
-        return transforms
-
-
-class ImageReader(RecordTransformer):
-    """ Convert path into image array """
-
-    def __init__(self, config: Config) -> None:
-        super().__init__('cam/image_array', config, True)
-
-    def transform(self, record: Record, val: Any) -> Any:
-        if type(val) is str:
-            base_path = record['_image_base_path']
-            # only transform once into numpy img, when value is path to image
-            image_path = os.path.join(base_path, val)
-            image = load_image_arr(image_path, self.config)
-            return image
-        else:
-            return val
-
-
-class ImageNormalizer(RecordTransformer):
-    """ Normalize Images from np.uint8 to np.float32. We don't want to cache
-        these as they require 4x memory. """
-
-    def __init__(self, config: Config) -> None:
-        super().__init__('cam/image_array', config, False)
-
-    def transform(self, record: Record, val: np.ndarray) -> np.ndarray:
-        return normalize_image(val)
-
-
-class LazyRecord(object):
-    """ Lazy record which wraps around record dictionary. There is no
-        additional functionality here, it's all in the derived classes. """
-
-    def __init__(self,
-                 record: Record,
-                 model: KerasPilot,
-                 transforms: List[RecordTransformer]) -> None:
-        self.model = model
-        self.record = record
-        self.transforms = transforms
-
-    def get_x(self) -> Any:
-        """ Dispatch returning of X to the model which will call
-            get_entry() on the LazyRecord and from there drive the
-            transformations. """
-        return self.model.lazy_record_transform_x(self)
-
-    def get_y(self) -> Any:
-        """ Dispatch returning of Y to the model which will call
-            get_entry() on the LazyRecord and from there drive the
-            transformations. """
-        return self.model.lazy_record_transform_y(self)
-
-    def get_entry(self, key: str) -> Any:
-        """ Get entry from record and run through transformations """
-        val = self.record.get(key)
-        for transform in self.transforms:
-            val = transform.get(self.record, key, val)
-        return val
-
-
-class TubSequence1(Sequence):
-    """ Converts sequence of records to lazy records. """
-    def __init__(self,
-                 model: KerasPilot,
-                 config: Config,
-                 records: Records,
-                 is_train: bool = True) -> None:
-        self.model = model
-        self.config = config
-        self.records = records
-        cfg_attr = 'TRAIN_TRANSFORMATIONS' if is_train else \
-            'VALIDATION_TRANSFORMATIONS'
-        transforms = getattr(self.config, cfg_attr, DEFAULT_TRANSFORMATIONS)
-        self.transformations = RecordTransformer.create(transforms, self.config)
-
-    def __len__(self) -> int:
-        return len(self.records)
-
-    def __getitem__(self, index: int) -> LazyRecord:
-        record = self.records[index]
-        lazy_record = LazyRecord(record, self.model, self.transformations)
-        return lazy_record
-
-
-class BatchSequence1(TfSequence):
-    def __init__(self,
-                 lazy_records: TubSequence,
+                 pipeline: Iterable[Tuple[X, Y]],
                  batch_size: int) -> None:
-        self.lazy_records = lazy_records
+        self.pipeline = pipeline
         self.batch_size = batch_size
 
-    def __len__(self) -> float:
-        return len(self.lazy_records) // self.batch_size
+    def __len__(self) -> int:
+        return math.ceil(len(self.pipeline) / self.batch_size)
 
     def __getitem__(self, index: int) -> Tuple[Any, Any]:
         count = 0
@@ -199,11 +39,9 @@ class BatchSequence1(TfSequence):
         # collecting across the whole batch
         while count < self.batch_size:
             i = (index * self.batch_size) + count
-            if i >= len(self.lazy_records):
+            if i >= len(self.pipeline):
                 break
-            this_record = self.lazy_records[i]
-            single_x = this_record.get_x()
-            single_y = this_record.get_y()
+            single_x, single_y = self.pipeline[i]
             x.append(single_x)
             y.append(single_y)
             count += 1
@@ -227,47 +65,12 @@ class BatchSequence1(TfSequence):
         return x_res, y_res
 
 
-
-
-
-
-#### new stuff here
-
-class BatchSequence(TfSequence):
-    def __init__(self,
-                 pipeline: Iterable[Tuple[X, Y]],
-                 batch_size: int) -> None:
-        self.pipeline = pipeline
-        self.batch_size = batch_size
-
-    def __len__(self) -> int:
-        return len(self.pipeline) // self.batch_size
-
-    def __getitem__(self, index: int) -> Tuple[Any, Any]:
-        count = 0
-        x = []
-        y = []
-        # collecting across the whole batch
-        while count < self.batch_size:
-            i = (index * self.batch_size) + count
-            if i >= len(self.pipeline):
-                break
-            single_x, single_y = self.pipeline[i]
-            x.append(single_x)
-            y.append(single_y)
-            count += 1
-
-        x_res = np.array(x)
-        y_res = np.array(y)
-        return x_res, y_res
-
-
 def make_tf_data(pipeline, batch_size):
     gen = lambda: pipeline
     dataset = tf.data.Dataset.from_generator(
         generator=gen,
-        output_types=(tf.float64, tf.float64))
-        #output_shapes=(tf.TensorShape([120, 160, 3]), tf.TensorShape([2,])))
+        output_types=(tf.float64, tf.float64),
+        output_shapes=(tf.TensorShape([120, 160, 3]), tf.TensorShape([2,])))
 
     return dataset.repeat().batch(batch_size)
 
@@ -299,69 +102,61 @@ def train(cfg: Config,
         print(kl.model.summary())
 
     batch_size: int = cfg.BATCH_SIZE
-    # # loading all records into a single data set
-    # dataset = TubDataset(tub_paths, config=cfg)
-    # records = dataset.load_records()
-    # training_records, validation_records \
-    #     = train_test_split(records, shuffle=False,
-    #                        test_size=(1. - cfg.TRAIN_TEST_SPLIT))
-    # print('Records # Training %s' % len(training_records))
-    # print('Records # Validation %s' % len(validation_records))
-    #
-    # # step 1 of pipeline, create the sequence:
-    # training = TubSequence(records=training_records)
-    # validation = TubSequence(records=validation_records)
-    #
-    # # step 2 of pipeline, extract X, Y sequence from data
-    # # get X from tub record:
-    # def get_X(t: TubRecord) -> np.ndarray:
-    #     img_arr = t.image(cached=True, normalize=True)
-    #     return img_arr
-    #
-    # def get_Y(t: TubRecord) -> np.ndarray:
-    #     y1 = t.underlying['user/angle']
-    #     y2 = t.underlying['user/throttle']
-    #     return np.array([y1, y2])
-    #
-    # # TODO: training_pipe iterates only once and then is exhausted. That's
-    # #  why keras training fails after one epoch.
-    # # training_pipe = training.build_pipeline(get_X, get_Y)
-    # # validation_pipe = validation.build_pipeline(get_X, get_Y)
-    #
-    # # # this version is working.
-    # training_pipe = Pipeline(training, get_X, get_Y)
-    # validation_pipe = Pipeline(training, get_X, get_Y)
-    #
-    # # step 3 of pipeline, transform into tf.data or tf.sequence
-    # # using tf.Data disabled.
-    # # dataset_train = make_tf_data(training_pipe, cfg.BATCH_SIZE)
-    # # dataset_validate = make_tf_data(validation_pipe, cfg.BATCH_SIZE)
-    #
-    # dataset_train = BatchSequence(training_pipe, cfg.BATCH_SIZE)
-    # dataset_validate = BatchSequence(validation_pipe, cfg.BATCH_SIZE)
-
-    # now compare to the legacy sequences
-    dataset1 = TubDataset1(tub_paths, shuffle=False,
+    # loading all records into a single data set
+    dataset = TubDataset(tub_paths, config=cfg)
+    records = dataset.load_records()
+    training_records, validation_records \
+        = train_test_split(records, shuffle=True,
                            test_size=(1. - cfg.TRAIN_TEST_SPLIT))
+    print('Records # Training %s' % len(training_records))
+    print('Records # Validation %s' % len(validation_records))
 
-    training_records1, validation_records1 = dataset1.train_test_split()
-    print('Records # Training %s' % len(training_records1))
-    print('Records # Validation %s' % len(validation_records1))
-    training1 = TubSequence1(model=kl, config=cfg, records=training_records1)
-    validation1 = TubSequence1(model=kl, config=cfg, records=validation_records1)
+    # step 1 of pipeline, create the sequence:
+    training = TubSequence(records=training_records)
+    validation = TubSequence(records=validation_records)
 
-    dataset_train1 = BatchSequence1(training1, cfg.BATCH_SIZE)
-    dataset_validate1 = BatchSequence1(validation1, cfg.BATCH_SIZE)
+    # step 2 of pipeline, extract X, Y sequence from data
+    # get X from tub record:
+    def get_X(t: TubRecord) -> np.ndarray:
+        img_arr = t.image(cached=True, normalize=True)
+        return img_arr
 
-    assert len(dataset_validate1) > 0, \
+    def get_Y(t: TubRecord) -> Tuple[float, float]:
+        y1 = t.underlying['user/angle']
+        y2 = t.underlying['user/throttle']
+        return y1, y2
+
+    # TODO: training_pipe iterates only once and then is exhausted. That's
+    #  why keras training fails after one epoch.
+    # training_pipe = training.build_pipeline(get_X, get_Y)
+    # validation_pipe = validation.build_pipeline(get_X, get_Y)
+
+    # # this version is working.
+    training_pipe = Pipeline(training, get_X, get_Y)
+    validation_pipe = Pipeline(validation, get_X, get_Y)
+
+    # step 3 of pipeline, transform into tf.data or tf.sequence
+    # using tf.Data disabled
+    # dataset_train = make_tf_data(training_pipe, cfg.BATCH_SIZE)
+    # dataset_validate = make_tf_data(validation_pipe, cfg.BATCH_SIZE)
+    # train_size = math.ceil(len(training_pipe) / cfg.BATCH_SIZE)
+    # val_size = math.ceil(len(validation_pipe) / cfg.BATCH_SIZE)
+
+    # here using tf.sequence
+    dataset_train = BatchSequence(training_pipe, cfg.BATCH_SIZE)
+    dataset_validate = BatchSequence(validation_pipe, cfg.BATCH_SIZE)
+    train_size = len(dataset_train)
+    val_size = len(dataset_validate)
+
+    assert val_size > 0, \
         "Not enough validation data, decrease the batch size or add more data."
 
     history = kl.train(model_path=output_path,
-                       train_data=dataset_train1,
-                       train_steps=len(dataset_train1),
+                       train_data=dataset_train,
+                       train_steps=train_size,
                        batch_size=batch_size,
-                       validation_data=dataset_validate1,
-                       validation_steps=len(dataset_validate1),
+                       validation_data=dataset_validate,
+                       validation_steps=val_size,
                        epochs=cfg.MAX_EPOCHS,
                        verbose=cfg.VERBOSE_TRAIN,
                        min_delta=cfg.MIN_DELTA,
